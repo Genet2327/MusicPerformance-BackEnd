dbe5958933b9ea0c49bb0ab8a09882be24ec9747
Genet2327 <79076435+Genet2327@users.noreply.github.com>
2023-03-10 18:22:34 -0600

    Update server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ const db = require("./app/models");
 const Role = db.role;
 
 //db.sequelize.sync();
-
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
+ db.sequelize.sync({force: true}).then(() => {
+ console.log('Drop and Resync Db');
+ initial();
+ });
 
 
 
