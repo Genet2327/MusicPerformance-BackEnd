b42cd04a1262aaa07dfc890a70bd8832f5acbbb7
Genet2327 <79076435+Genet2327@users.noreply.github.com>
2023-04-19 23:55:53 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,13 +9,11 @@ const db = require("./app/models");
 const Role = db.role;
 
 //db.sequelize.sync();
-db.sequelize.sync({ force: true }).then(() => {
-  console.log("Drop and re-sync db.");
-});
-//  db.sequelize.sync({ force: true }).then(() => {
-//    console.log("Drop and Resync Db");
-//    initial();
-//  });
+
+ db.sequelize.sync({ force: true }).then(() => {
+   console.log("Drop and Resync Db");
+   initial();
+ });
 
 var corsOptions = {
   origin: "http://localhost:8081",
